5416855205a441a989ec897acd3876a7702d825a
Murshid Bin Ibrahim <murshidshazz@gmail.com>
2023-02-01 01:07:04 +0530

    User module finished

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 const userRoute = require('./routes/userRoute');
 app.use('/',userRoute);
 
+
 app.listen(3000, ()=>{
     console.log("Server running...");
 })
--- controllers/userController.js
+++ controllers/userController.js
@@ -58,9 +58,87 @@ const insertUser = async(req , res) => {
     }
 }
 
+//login method for users
+
+const loginLoad = async(req, res) => {
+    try {
+
+        res.render('login')
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const verifyLogin = async(req,res) => {
+
+    try {
+        
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const userData = await User.findOne({email:email});
+
+        if (userData) {
+
+            const passwordMatch = await bcrypt.compare(password, userData.password);
+
+            if(passwordMatch){
+
+                req.session.user_id = userData._id;
+
+                res.redirect('/home')
+
+            }
+            else{
+
+                res.render('login', {message : "Email and password is incorrect"});
+            }
+            
+        } else {
+
+            res.render('login', {message : "Please provide your Email and password is incorrect"});
+
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const loadHome = async (req,res)=> {
+
+        try {
+            
+            const userData = await User.findById({_id : req.session.user_id});
+            res.render('home',{user : userData});
+
+        } catch (error) {
+
+            console.log(error.message);
+            
+        }
+}
+
+const userLogout = async(req,res) =>{
+    try {
+
+        req.session.destroy();
+        res.redirect('/');
+        
+    } catch (error) {
+
+        console.log(error.message);
+    }
+}
+
 module.exports = {
 
     loadRegister,
-    insertUser
+    insertUser,
+    loginLoad,
+    verifyLogin,
+    loadHome,
+    userLogout
 
 }
