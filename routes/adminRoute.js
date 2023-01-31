3045ad38a4a8f7c3a8edb8e45032b10dcca664d9
Murshid Bin Ibrahim <murshidshazz@gmail.com>
2023-02-01 02:40:43 +0530

    Admin Login & Logout Completed

---
+++ routes/adminRoute.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const admin_route = express();
+
+const session = require('express-session');
+const config = require('../config/config');
+
+const bodyParser = require('body-parser');
+
+admin_route.use(bodyParser.json());
+admin_route.use(bodyParser.urlencoded({extended : true}));
+
+admin_route.set('view engine', 'ejs');
+admin_route.set('views', './views/admin');
+
+const auth = require('../middleware/adminAuth');
+
+const adminController = require('../controllers/adminController');
+
+
+admin_route.get('/', auth.isLogout, adminController.loadLogin);
+
+admin_route.post('/', adminController.verifyLogin);
+
+admin_route.get('/home', auth.isLogin, adminController.loadDashboard);
+
+admin_route.get('/logout', auth.isLogout, adminController.logout)
+
+admin_route.get('*', function (req,res) {
+
+    res.redirect('/admin')
+    
+})
+
+module.exports = admin_route;
